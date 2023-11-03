c9e556cf7a486f1ff1d112970c588cf3139cf45a
Max <tehcmanmax@gmail.com>
2023-11-03 13:33:27 +0100

    fix/undo 2 last commits

--- public/js/config.js
+++ public/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
   cognito: {
-    userPoolId: "eu-north-1_7dwlQt4Ru", // e.g. eu-east-2_uXboG5pAb
-    userPoolClientId: "2rqjme50eje2fmoa58jnrb9ggm", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: "eu-north-1", // e.g. us-east-2
+    userPoolId: "eu-west-1_PPkynf3wh", // e.g. eu-east-2_uXboG5pAb
+    userPoolClientId: "16kjssnfp8bm7v6ugksic72rl0", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "eu-west-1", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "https://pgfre02331.execute-api.eu-north-1.amazonaws.com/prod", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
   },
 }
