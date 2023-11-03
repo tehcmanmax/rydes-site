26f9daa3acb2eefb45413aec6c13fd3f279960d8
Max <tehcmanmax@gmail.com>
2023-11-03 13:56:01 +0100

    fix/return-api-invoke-url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Add your API invoke URL below, configured in Module 3. 
 
 module.exports = {
-    api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
-    }
+  api: {
+    invokeUrl: "https://pgfre02331.execute-api.eu-north-1.amazonaws.com/prod", // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+  },
 }
