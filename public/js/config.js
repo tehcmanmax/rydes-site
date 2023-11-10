db574e3d91d3b5648c55309394ad91bb3d29c1ff
Max <tehcmanmax@gmail.com>
2023-11-10 19:47:51 +0100

    feat/redo

--- public/js/config.js
+++ public/js/config.js
@@ -1,11 +1,11 @@
-window._config = {
-  cognito: {
-    userPoolId: "eu-north-1_roGxaDbgV", // e.g. eu-east-2_uXboG5pAb
-    userPoolClientId: "44j7dd58d0olr759nhknoa93o8", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: "eu-north-1", // e.g. us-east-2
-  },
-  api: {
-    invokeUrl:
-      "arn:aws:cognito-idp:eu-north-1:782385805622:userpool/eu-north-1_7dwlQt4Ru", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
-  },
-}
+// window._config = {
+//   cognito: {
+//     userPoolId: "eu-north-1_roGxaDbgV", // e.g. eu-east-2_uXboG5pAb
+//     userPoolClientId: "44j7dd58d0olr759nhknoa93o8", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+//     region: "eu-north-1", // e.g. us-east-2
+//   },
+//   api: {
+//     invokeUrl:
+//       "arn:aws:cognito-idp:eu-north-1:782385805622:userpool/eu-north-1_7dwlQt4Ru", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+//   },
+// }
