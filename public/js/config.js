6c314731ebc5454c9a34422108fce2b6d1742eee
Max <tehcmanmax@gmail.com>
2023-11-10 16:51:18 +0100

    feat/cognito-configured

--- public/js/config.js
+++ public/js/config.js
@@ -1,10 +1,11 @@
 window._config = {
   cognito: {
-    userPoolId: "eu-west-1_PPkynf3wh", // e.g. eu-east-2_uXboG5pAb
-    userPoolClientId: "16kjssnfp8bm7v6ugksic72rl0", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-    region: "eu-west-1", // e.g. us-east-2
+    userPoolId: "eu-north-1_7dwlQt4Ru", // e.g. eu-east-2_uXboG5pAb
+    userPoolClientId: "4g5rlfjpmsepjnbl2a21pvhul", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "eu-north-1", // e.g. us-east-2
   },
   api: {
-    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    invokeUrl:
+      "arn:aws:cognito-idp:eu-north-1:782385805622:userpool/eu-north-1_7dwlQt4Ru", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
   },
 }
